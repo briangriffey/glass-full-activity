d2bf9c26ed7acfa5625248b7aae9678588930e1b
briangriffey <brian@briangriffey.com>
2014-01-05 18:00:58 -0600

    added some cards

--- src/com/briangriffey/glass/example/CardActivity.java
+++ src/com/briangriffey/glass/example/CardActivity.java
@@ -3,8 +3,11 @@ package com.briangriffey.glass.example;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.glass.app.Card;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
 
 /**
  * Created by briangriffey on 1/3/14.
@@ -15,12 +18,44 @@ public class CardActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Create a card with some simple text and a footer.
-        Card card1 = new Card(this);
-        card1.setText("This card has a footer.");
-        card1.setFootnote("I'm the footer!");
-        View card1View = card1.toView();
 
-        setContentView(card1View);
+        CardScrollView scrollView = new CardScrollView(this);
+        scrollView.setAdapter(new SpecialCardAdapter());
+        scrollView.activate();
+
+        setContentView(scrollView);
+    }
+
+    private class SpecialCardAdapter extends CardScrollAdapter {
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return new Integer(i);
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            // Create a card with some simple text and a footer.
+            Card card1 = new Card(CardActivity.this);
+            card1.setText("This is card " + i);
+            return card1.toView();
+
+        }
+
+        @Override
+        public int findIdPosition(Object o) {
+            return (Integer)o;
+        }
+
+        @Override
+        public int findItemPosition(Object o) {
+            return (Integer)o;
+        }
     }
 }
